382fc4abfbec44357624bcb178351a809a7d5bfb
AkhyarAzamta <bebyraska14@gmail.com>
2022-11-18 10:42:57 +0700

    body-parser untuk menerima form input

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // deklarasi apa saja yang dibutuhkan lalu install npm i express mysql
 const express = require("express") //untuk mengatur fungsionalitas website, seperti pengelolaan routing dan session, permintaan HTTP, penanganan error, serta pertukaran data di server
 const mysql = require("mysql") //untuk koneksi ke mysql
+const BodyParser = require("body-parser")
 
 const app = express(); //definisikan si express nya menjadi app karna awalnya sebuah function
+
+app.use(BodyParser.urlencoded({ extended: true})) //untuk menerima form input
 
 app.set("view engine", "ejs") //ejs sebagai template engine
 app.set("views", "views") //untuk memberitau direktori html nya ada difolder views
@@ -24,9 +27,22 @@ db.connect((err) => {
     db.query(sql, (err, result) => {
         const users = JSON.parse(JSON.stringify(result))
         console.log("hasil database -> ", users)
+
+        //untuk get data
         app.get("/", (req, res) => {
             res.render("index", {users: users, title: "Data Mahasiswa Kelas B"})
         })
+
+        //untuk insert data
+        app.post("/tambah", (req, res) => {
+            const insertSql = 'INSERT INTO user (nama, nim) VALUES ('${req.body.nama}', '${req.body.nim}');'
+            db.query(insertSql, (err, result) => {
+                if (err) throw err
+                res.redirect("/")
+            })
+        })
+
+
     })
 
     
